19fc1cb7031534787ccab4277053d4fe584c1b51
KinerBoi <kinish2005@gmail.com>
2024-01-31 15:20:28 -0800

    changing some stuff
    changing to make the two methods work

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -15,4 +18,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2() {
+    int[] input = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testReverse32() {
+    int[] input = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input);
+  }
+
+
 }
